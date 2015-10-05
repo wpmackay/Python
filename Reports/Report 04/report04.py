bf3055bd02a6aa5fa1f759830dc2ad3e938637d5
William Mackay <wpmackay@buffalo.edu>
2015-10-05 12:52:10 -0400

    Oct. 5, 2015

---
+++ Reports/Report 04/report04.py
@@ -0,0 +1,59 @@
+#William Mackay
+#MTH 337 Assignment 04
+
+from pylab import *
+
+#Part 1- Loading and Plotting Weather Balloon Data
+data=loadtxt("balloon.dat", skiprows=138)
+mins=data[:,0]
+secs=data[:,1]
+t=mins+(secs/60)
+temp=data[:,5]
+
+plot(t,temp,'b.')
+xlabel('Time (mins)')
+ylabel('Temperature (deg. C)')
+title('Temperature vs. Time Measured by a Weather Balloon')
+
+#Part 2- Finding machine epsilon, maximum float, and minimum float
+def find_epsilon():
+    n=0
+    while True:
+        x=2**(-n)
+        if 1+x!=1:
+            n+=1
+        else:
+            return 2**-(n-1)
+
+def max_float():
+    n=1.
+    x=2.
+    count=0
+    while True:
+        y=x**n
+        if y>1.797*10**308 and y-x**(n-0.001)<10**306:
+            return y
+        else:
+            n+=.001
+            count+=1
+            
+def min_float():
+    n=1.
+    x=2.
+    while True:
+        y=x**n
+        #print '%e' % y
+        if y==x**(n+1):
+            return y
+        else:
+            n-=0.015
+            
+epsilon=find_epsilon()
+minFloat=min_float()
+maxFloat=max_float()
+
+print 'Machine epsilon is',epsilon
+print 'The smallest floating point number is', minFloat
+print 'The largest floating point number is', maxFloat
+
+show()
